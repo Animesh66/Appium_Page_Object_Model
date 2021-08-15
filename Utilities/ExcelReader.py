dbeb611537ffc005357b78c2263a8f64a20bf8ba
Animesh <er.animesh6@hotmail.com>
2021-08-15 22:33:27 +0530

    Utility files created.

--- Utilities/ExcelReader.py
+++ Utilities/ExcelReader.py
@@ -26,7 +26,7 @@ def setCellData(path, sheetName, rowNum, colNum, data):
     workbook.save(path)
 
 
-path = "..//excel//testdata.xlsx"
+path = "./Excel/testdata.xlsx"
 sheetName = "LoginTest"
 
 rows = getRowCount(path,sheetName)
