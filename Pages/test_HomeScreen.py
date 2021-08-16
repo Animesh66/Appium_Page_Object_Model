91841160ffa217824f9080fbb87c13d0cb3e4cb6
Animesh <er.animesh6@hotmail.com>
2021-08-16 22:08:29 +0530

    page class files created.

--- Pages/test_HomeScreen.py
+++ Pages/test_HomeScreen.py
@@ -5,11 +5,11 @@ class TestHomeScreen(TestBasePage):
     def __init__(self, driver):
         super().__init__(driver)
 
-    def go_to_hotels(self):
+    def test_go_to_hotels(self):
         self.click("hotels_xpath")
 
-    def go_to_villas(self):
+    def test_go_to_villas(self):
         self.click("villa_xpath")
 
-    def got_to_trains(self):
+    def test_got_to_trains(self):
         pass
--- Pages/test_HotelScreen.py
+++ Pages/test_HotelScreen.py
@@ -5,3 +5,11 @@ class TestHotelScreen(TestBasePage):
 
     def __init__(self, driver):
         super().__init__(driver)
+
+    def test_search_hotels(self, city):
+        self.click("search_button_xpath")
+        self.click("search_anywhere_xpath")
+        self.type("search_box_id", "Delhi")
+        self.click_index("com.goibibo:id/lytLocationItem",0)
+        self.click("search_button_xpath")
+
