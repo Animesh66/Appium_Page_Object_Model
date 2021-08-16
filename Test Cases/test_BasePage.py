c2b438351c037962d16907a00f4a224db4682f15
Animesh <er.animesh6@hotmail.com>
2021-08-16 13:11:39 +0530

    Utility files created.

---
+++ Test Cases/test_BasePage.py
@@ -0,0 +1,17 @@
+import logging
+from Utilities.LogUtil import Logger
+from Utilities import configReader
+
+
+log = Logger(__name__, logging.INFO)
+class BasePage:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def click(self, locator):
+        if str(locator).endswith('xpath'):
+            self.driver.find_element_by_xpath(configReader.readConfig("locators", locator)).click()
+
+        elif str(locator).endswith('id'):
+            self.driver.find_element_by_id(configReader.readConfig("locators", locator)).click()
