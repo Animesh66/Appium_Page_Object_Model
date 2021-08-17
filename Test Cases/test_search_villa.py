36170102260095ac551996a13dcbca8725f8da56
Animesh <er.animesh6@hotmail.com>
2021-08-17 16:17:50 +0530

    Second test case added.

--- Test Cases/test_search_villa.py
+++ Test Cases/test_search_villa.py
@@ -8,7 +8,7 @@ from Utilities import dataProvider
 class Test_SearchVillas(BaseTest):
 
     @pytest.mark.parametrize("city", dataProvider.get_data("LoginTest"))
-    def test_search_hotels(self, city):
+    def test_search_villas(self, city):
         home_screen = HomeScreen(self.driver)
         home_screen.go_to_villas().search_villa(city)  # this is method chaining
 
