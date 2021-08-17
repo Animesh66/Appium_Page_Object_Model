1420f66c141110be6eab14ddcbc57fcfef5b9827
Animesh <er.animesh6@hotmail.com>
2021-08-17 20:28:12 +0530

    Second test case COMPLETED.

--- Test_Cases/BaseTest.py
+++ Test_Cases/BaseTest.py
@@ -1,5 +1,7 @@
 import pytest
 
+
+@pytest.mark.flaky(reruns=3)  # this wil rerun the failed test case 3 times
 @pytest.mark.usefixtures("log_on_failure", "appium_driver")
 class BaseTest:
     pass
