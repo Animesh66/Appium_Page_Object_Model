da268b15a3e8310b17ee48888758b9c96b0ceb9c
Animesh <er.animesh6@hotmail.com>
2021-08-17 12:34:20 +0530

    first test case files created.

--- Utilities/configReader.py
+++ Utilities/configReader.py
@@ -3,5 +3,5 @@ from configparser import ConfigParser
 
 def readConfig(section, key):
     config = ConfigParser()
-    config.read("./Configurations/conf.ini")
+    config.read("/Users/animeshmukherjee/PycharmProjects/pythonProject/Appium_Page_Object_Model/Configurations/conf.ini")
     return config.get(section, key)
