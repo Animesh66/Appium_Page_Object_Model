2d73406d9650f5d0170a16d7976c9d25c0160b8f
Animesh <er.animesh6@hotmail.com>
2021-08-17 15:55:25 +0530

    first test case files created.

--- Pages/BasePage.py
+++ Pages/BasePage.py
@@ -17,7 +17,7 @@ class BasePage:
         elif str(locator).endswith("_id"):
             self.driver.find_element_by_id(configReader.readConfig("locators", locator)).click()
 
-        #log.logger.info("Clicking on the element: " + str(locator))
+        log.logger.info("Clicking on the element: " + str(locator))
 
     def click_index(self, locator, index):
         if str(locator).endswith("_xpath"):
@@ -26,7 +26,7 @@ class BasePage:
         elif str(locator).endswith("_id"):
             self.driver.find_elements_by_id(configReader.readConfig("locators", locator))[index].click()
 
-        #log.logger.info("Clicking on the element: " + str(locator) + " with index " + str(index))
+        log.logger.info("Clicking on the element: " + str(locator) + " with index " + str(index))
 
     def type(self, locator, value):
         if str(locator).endswith("_xpath"):
@@ -35,7 +35,7 @@ class BasePage:
         elif str(locator).endswith("_id"):
             self.driver.find_element_by_id(configReader.readConfig("locators", locator)).send_keys(value)
 
-        #log.logger.info("Typing on the element: " + str(locator) + " entered the value as: " + str(value))
+        log.logger.info("Typing on the element: " + str(locator) + " entered the value as: " + str(value))
 
     def get_text(self, locator):
         if str(locator).endswith("_xpath"):
@@ -44,5 +44,5 @@ class BasePage:
         elif str(locator).endswith("_id"):
             text = self.driver.find_element_by_id(configReader.readConfig("locators", locator)).text
 
-        #log.logger.info("Getting text from an element: " + str(locator))
+        log.logger.info("Getting text from an element: " + str(locator))
         return text
