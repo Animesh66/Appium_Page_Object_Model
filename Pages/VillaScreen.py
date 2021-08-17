d574337fc66a199caa30b466327ad5a9242fdefa
Animesh <er.animesh6@hotmail.com>
2021-08-17 16:45:34 +0530

    Second test case COMPLETED.

--- Pages/VillaScreen.py
+++ Pages/VillaScreen.py
@@ -1,4 +1,6 @@
 from Pages.BasePage import BasePage
+from Utilities.scroll_util import ScrollUtil
+from Utilities import configReader
 
 
 class VillaScreen(BasePage):
@@ -9,10 +11,9 @@ class VillaScreen(BasePage):
     def search_villa(self, city):
         self.click("villa_search_xpath")
         self.type("search_box_id", city)
-        self.click_index("search_result_id",0)
+        self.click_index("search_result_id", 0)
         self.click("calender_start_date_xpath")
         self.click("calender_end_date_xpath")
         self.click("continue_button_xpath")
         self.click("view_stays_xpath")
-
-
+        self.scrollToTextByAndroidUIAutomator(configReader.readConfig("search_text", "searched_hotel_text"), self.driver)
